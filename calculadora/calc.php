71fe70a59fe0ca6e7371eb32b17c5aebfe61787d
dudu <nicolalmeida_se@outlook.com>
2024-08-19 22:54:09 -0300

    changes

--- calculadora/calc.php
+++ calculadora/calc.php
@@ -10,6 +10,57 @@
     $num1 = $_POST['num-1'];
     $num2 = $_POST['num-2'];
     $op = intval(readline());
+// echo "Calculadora interativa\n\n";
+// echo "Digite o primeiro numero\n";
+// $x = floatval(readline());
+
+// echo "Escolha a opcao desejada:\n(1)Adicao\n(2)Subtração\n(3)Multiplicação\n(4)Divisão\n";
+// $op = intval(readline());
+// switch($op){
+//     case 1:
+//         echo "Opcao escolhida Adicao\n\n";
+//         // echo "$x + $y = ".number_format(($x+$y), 2, ".", "")."\n";
+//         // $adicao = $x + $y;
+//         break;
+//     case 2:
+//         echo "Opcao escolhida Subtracao\n\n";
+//         // echo "$x - $y = ".number_format(($x-$y), 2, ".", "")."\n";
+//         // $subtracao = $x - $y;
+//         break;
+//     case 3:
+//         echo "Opcao escolhida Multiplicacao\n\n";
+//         // echo "$x x $y = ".number_format(($x*$y), 2, ".", "")."\n";
+//         // $multiplicacao = $x * $y;
+//         break;
+//     case 4:
+//         echo "Opcao escolhida Divisao\n\n";
+//         // if($y == 0){
+//         // echo "Erro de sintaxe! (divisao por zero)\n";
+//         // break;
+//         // }
+//         // else
+//         // echo "$x / $y = ".number_format(($x*$y), 2, ".", "")."\n";
+//         // $divisao = $x / $y;
+//         break;
+// }
+// echo "Digite o segundo numero\n";
+// $y = floatval(readline());
+// if($op == 1){
+//     echo "$x + $y = ".($x+$y)."\n";
+// }
+// else if($op == 2){
+//     echo "$x - $y = ".($x-$y)."\n";
+// }
+// else if($op == 3){
+//     echo "$x x $y = ".($x*$y)."\n";
+// }
+// else if($op == 4){
+//     if($y == 0){
+//         echo "Erro de sintaxe!! (divisao por zero)\n";
+//     }
+//     else
+//         echo "$x / $y = ".($x/$y)."\n";
+// }
     ?>
 </body>
 </html>
